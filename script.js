f3e2a1ce8bdfafda00599f3b152a93fe8e25e54e
douglas <douglasklemes0303@gmail.com>
2019-07-18 17:38:44 -0300

    Adicinar atividade de calculo de media em js

---
+++ script.js
@@ -0,0 +1,32 @@
+
+
+// let nome = prompt("digite seu nome");
+// let DataDeNascimento = Number(prompt("digite seu ano de nascimento"));
+// let ano = 2019;
+
+// idade = ano - DataDeNascimento;
+
+// console.log("Olá, eu me chamo " + nome + " eu tenho " + idade + " ano,  e estou estudando jasvascript")
+
+
+let nomeAluno=  prompt("digite seu nome");
+
+let matricula =  Number (prompt("digite seu numero de matricula"));
+
+let nota1  = Number (prompt("digite nota1"));
+
+let nota2  = Number (prompt("digite nota2"));
+
+let media = (nota1 + nota2)/2;
+
+if(media > 0 && media <= 10)  {
+    alert("O aluno(a) " + nomeAluno + " matriculada " + matricula + " obteve a media final " + media );
+} 
+else if( media === 0){
+    alert( "A Media Não Pode Ser 0")
+}
+else {
+    alert('A Media Não Pode Ser Maior Que 10');
+}
+
+
